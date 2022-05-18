9c9d2ce4495f2c061ac394e732c38c928b80de97
dhruv50ae <linkdp2012@gmail.com>
2022-05-18 13:57:54 +0100

    Added files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+});
+
+app.listen(3000, (req, res) => {
+  console.log("The server has started on port 3000");
+});
